006d84a262f41d4f8490fb24b1c5b0797d06ee17
Jacob <Jacob@KEPLER>
2023-01-08 21:43:40 -0600

    Add project files.

---
+++ PastaPalace/Customs/NoodleChain/RawNoodlePot.cs
@@ -0,0 +1,97 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PastaPalace.Customs.NoodleChain
+{
+    internal class RawNoodlePot : CustomItemGroup
+    {
+        public override string UniqueNameID => "RawNoodlePot";
+        public override GameObject Prefab => Mod.bundle.LoadAsset<GameObject>(UniqueNameID + "Model");
+        public override Item DisposesTo => Mod.Pot;
+        public override ItemCategory ItemCategory => ItemCategory.Generic;
+        public override ItemStorage ItemStorageFlags => ItemStorage.None;
+        public override List<ItemGroup.ItemSet> Sets => new List<ItemGroup.ItemSet>    
+        {
+            new ItemGroup.ItemSet
+            {
+                Max = 2,
+                Min = 2,
+                Items = new List<Item>
+                {
+                    Mod.Water,
+                    Mod.RawNoodles
+                }
+            },
+            new ItemGroup.ItemSet
+            {
+                Max = 1,
+                Min = 1,
+                Items = new List<Item>
+                {
+                    Mod.Pot
+                }
+            }
+        };
+        public override List<ItemGroup.ItemProcess> Processes => new List<Item.ItemProcess>
+        {
+            new Item.ItemProcess
+            {
+                Duration = 10,
+                Process = Mod.Cook,
+                Result = Mod.CookedNoodlePot
+            }
+        };
+
+        internal class RawNoodlePotModel : ItemGroupView
+        {
+            internal RawNoodlePotModel()
+            {
+                GameObject Prefab = Mod.bundle.LoadAsset<GameObject>("RawNoodlePotModel");
+
+                ComponentGroups = new List<ComponentGroup>() {
+                new ComponentGroup()
+                {
+                    Item = Mod.RawNoodles,
+                    GameObject = Prefab.transform.GetChild(0).Find("Spaghetti.003").gameObject
+                },
+
+                 new ComponentGroup()
+                {
+                    Item = Mod.Pot,
+                    GameObject = Prefab.transform.GetChild(0).Find("Pot").gameObject
+                },
+
+                  new ComponentGroup()
+                {
+                    Item = Mod.Water,
+                    GameObject = Prefab.transform.GetChild(0).Find("Water").gameObject
+                }
+
+            };
+            }
+        }
+
+        public override void OnRegister(GameDataObject gdo)
+        {
+            gdo.name = "Ingredient - Raw Noodle Pot";
+
+            Prefab.AddComponent<RawNoodlePotModel>();
+
+             MaterialUtils.ApplyMaterial<MeshRenderer>(Prefab, "Pot/Pot.001/Cylinder.001", new Material[] {
+                MaterialUtils.GetExistingMaterial("Metal")});
+
+             MaterialUtils.ApplyMaterial<MeshRenderer>(Prefab, "Pot/Pot.001/Cylinder.003", new Material[] {
+                MaterialUtils.GetExistingMaterial("Metal")});
+
+             MaterialUtils.ApplyMaterial<MeshRenderer>(Prefab, "Water", new Material[] {
+               MaterialUtils.GetExistingMaterial("Water")});
+
+             MaterialUtils.ApplyMaterial<MeshRenderer>(Prefab, "Spaghetti.002/Spaghetti.003", new Material[] {
+               MaterialUtils.GetExistingMaterial("Bread - Bun")});
+        }
+    }
+}
---
+++ PastaPalace/Customs/NoodleChain/RawNoodles.cs
@@ -0,0 +1,38 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenLib.Customs;
+using KitchenLib.Utils;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PastaPalace.Customs.NoodleChain
+{
+    internal class RawNoodles : CustomItemGroup
+    {
+        public override string UniqueNameID => "RawNoodles";
+        public override GameObject Prefab => Mod.bundle.LoadAsset<GameObject>(UniqueNameID + "Model");
+        public override ItemCategory ItemCategory => ItemCategory.Generic;
+        public override ItemStorage ItemStorageFlags => ItemStorage.StackableFood;
+        public override List<ItemGroup.ItemSet> Sets => new List<ItemGroup.ItemSet>()
+        {
+            new ItemGroup.ItemSet()
+            {
+                Max = 2,
+                Min = 2,
+                Items = new List<Item>()
+                {
+                    Mod.Flour,
+                    Mod.EggCracked
+                }
+            }
+        };
+
+        public override void OnRegister(GameDataObject gdo)
+        {
+            gdo.name = "Ingredient - Raw Noodles";
+
+            MaterialUtils.ApplyMaterial<MeshRenderer>(Prefab, "Spaghetti/Spaghetti.001", new Material[] {
+                MaterialUtils.GetExistingMaterial("Bread - Bun")});
+        }
+    }
+}
